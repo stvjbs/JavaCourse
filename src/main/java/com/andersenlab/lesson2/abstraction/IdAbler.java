b524e115501327411c8d4e7aeaba9730ff9cd6b5
stvjbs <yarashukartem@icloud.com>
2024-06-12 22:30:50 +0200

    feature/lesson4: add IdAbler class
    1. Task finished. It's better to use abstract class, because we need to inherit both behavior and state.

---
+++ src/main/java/com/andersenlab/lesson2/abstraction/IdAbler.java
@@ -0,0 +1,13 @@
+package com.andersenlab.lesson2.abstraction;
+
+public abstract class IdAbler {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
