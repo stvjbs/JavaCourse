6daed6c58a6c2d5644695928120eb7fcd1afb635
stvjbs <yarashukartem@icloud.com>
2024-07-03 20:59:09 +0200

    feature/lesson8: add hibernate support

---
+++ lesson8/src/main/java/com/andersenlab/hibernate/dao/UserDAOHibernate.java
@@ -0,0 +1,35 @@
+package com.andersenlab.hibernate.dao;
+
+import com.andersenlab.dao.impl.UserCRUDable;
+import com.andersenlab.entity.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class UserDAOHibernate implements UserCRUDable {
+    @Override
+    public boolean saveUser(User user) {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(user);
+        tx.commit();
+        session.close();
+        return true;
+    }
+
+    @Override
+    public User getUserById(int id) {
+        return SessionFactoryProvider
+                .getSessionFactory()
+                .openSession()
+                .get(User.class, id);
+    }
+
+    @Override
+    public void deleteUserById(int id) {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        session.delete(session.get(User.class, id));
+        tx.commit();
+        session.close();
+    }
+}
