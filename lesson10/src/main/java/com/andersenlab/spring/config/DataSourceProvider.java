b886d41ca6a7a08faef3e13ac23a01a41db92f63
stvjbs <yarashukartem@icloud.com>
2024-07-11 17:29:30 +0200

    feature/lesson12: initial commit

---
+++ lesson10/src/main/java/com/andersenlab/spring/config/DataSourceProvider.java
@@ -0,0 +1,24 @@
+package com.andersenlab.spring.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ConditionalOnProperty(name = "spring.datasource.url")
+public class DataSourceProvider {
+    private static final String url = "jdbc:postgresql://localhost:5432/my_ticket_service_db";
+    private static final String user = "user";
+    private static final String password = "password";
+
+    public DataSourceProvider() {
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        return new DriverManagerDataSource(url, user, password);
+    }
+}
